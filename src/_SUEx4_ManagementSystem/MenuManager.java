5dc8cde7288f27179779785da34cb3fa72f33225
sueyeon <same2108@naver.com>
2021-03-12 16:05:36 +0900

    removed hello.java and added menumanager.java

--- src/_SUEx4_ManagementSystem/MenuManager.java
+++ src/_SUEx4_ManagementSystem/MenuManager.java
@@ -3,7 +3,11 @@ package _SUEx4_ManagementSystem;
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("1. -");
+		System.out.println("2. -");
+		System.out.println("3. -");
+		System.out.println("4. -");
+		System.out.println("5. -");
 
 	}
 
